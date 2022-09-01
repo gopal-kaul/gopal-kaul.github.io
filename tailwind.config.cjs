0360a65b01182c0cfeb5595a027ab52a0632752f
Gopal Kaul <gopalkaul5@gmail.com>
2022-09-02 00:05:31 +0530

    Add custom colors to tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#b0cdf1",
+        background: "#081426",
+        neonBlue: "#54e0c1",
+      },
+    },
+  },
+  plugins: [],
+};
