080ec20928a86ecb4cdac67aa8656bfb4c8b3f55
Gopal Kaul <gopalkaul5@gmail.com>
2022-10-21 10:57:21 +0530

    Fix the look slightly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="pt-32 md:pt-40 text-white">
         <h1 className="text-xl text-neonBlue">Hey, my name is</h1>
         <h1 className="text-6xl md:text-7xl">Gopal Kaul.</h1>
-        <div className="flex flex-row text-3xl md:text-4xl whitespace-nowrap">
+        <div className="flex flex-row text-2xl md:text-4xl whitespace-nowrap">
           <h2>I&apos;m a</h2> &nbsp;
           <Typewriter
             options={{
@@ -21,7 +21,7 @@ export default function Home() {
             }}
           />
         </div>
-        <h2 className="pt-16 text-xl md:text-3xl text-light">
+        <h2 className="pt-16 text-xl md:text-2xl text-light">
           I&apos;m a Computer Science Engineer pursuing my degree from Osmania
           University, Hyderabad. <br />I am a Full Stack Web Developer who loves
           React. I also am an avid Arch Linux user.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 export default function Navbar() {
   return (
-    <div className="mx-2 md:mx-6 mt-4 flex flex-row justify-between sticky text-light">
+    <div className="px-2 md:px-6 top-0 left-0 pt-4 flex flex-row justify-between fixed text-light w-full">
       <h1></h1>
       <div className="flex flex-row gap-4">
         <a href="#about">About Me</a>
