bb905ca76d0c60a738fde60bb255746e26bd434b
Gopal Kaul <gopalkaul5@gmail.com>
2022-10-21 20:55:02 +0530

    Minor color change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,21 +7,23 @@ export default function Home() {
         <h1 className="text-6xl md:text-7xl">Gopal Kaul.</h1>
         <div className="flex flex-row text-2xl md:text-4xl whitespace-nowrap">
           <h2>I&apos;m a</h2> &nbsp;
-          <Typewriter
-            options={{
-              autoStart: true,
-              strings: [
-                "Full Stack Developer.",
-                "JavaScript Developer.",
-                "Gamer.",
-                "Linux Enthusiast.",
-              ],
-              loop: true,
-              delay: 75,
-            }}
-          />
+          <div className="text-light">
+            <Typewriter
+              options={{
+                autoStart: true,
+                strings: [
+                  "Full Stack Developer.",
+                  "JavaScript Developer.",
+                  "Gamer.",
+                  "Linux Enthusiast.",
+                ],
+                loop: true,
+                delay: 75,
+              }}
+            />
+          </div>
         </div>
-        <h2 className="pt-16 text-xl md:text-2xl text-light">
+        <h2 className="pt-16 text-xl md:text-2xl text-white">
           I&apos;m a Computer Science Engineer pursuing my degree from Osmania
           University, Hyderabad. <br />I am a Full Stack Web Developer who loves
           React. I also am an avid Arch Linux user.
