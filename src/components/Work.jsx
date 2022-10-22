2d7a5272add174798aa077563d66d8352a5fdbbb
Gopal Kaul <gopalkaul5@gmail.com>
2022-10-22 21:50:14 +0530

    Add Placement data

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,34 +6,54 @@ export default function Work() {
       company: "Oracle",
       role: "PDO Developer",
       duration: "June 2023",
-      info: ["Point 1", "Point 2", "Point 3"],
+      info: [
+        "PDO, Oracle CAGBU @18.2LPA INR",
+        "Placed under campus placement",
+        "First to get placed in the entire college",
+      ],
+    },
+    {
+
+      company: "Accellor(previously PopcornApps)",
+      role: "Intern",
+      duration: "September 2023 - Present",
+      info: [
+        "Worked on projects using Strapi and NestJS",
+      ],
     },
     {
       company: "SkilZen",
       role: "Frontend Engineer",
-      duration: "May 2022 to August 2022",
-      info: ["Point 1", "Point 2", "Point 3"],
+      duration: "May 2022 - August 2022",
+      info: [
+        "Worked with NextJS, Tailwind and Directus",
+        "Built https://collegeking.dev/",
+        "Also worked with integrating backend data to frontend",
+      ],
     },
     {
       company: "PopcornApps",
       role: "Intern",
       duration: "April 2021 to April 2022",
-      info: ["Point 1", "Point 2", "Point 3"],
+      info: ["Worked on Tableau, NodeJS and SQL" ],
     },
   ];
   return (
     <div className="min-h-screen text-white">
       <div className="pt-32">
-        <h3 className="text-3xl text-light">Previous Work Experience</h3>
+        <div className="flex flex-row items-center gap-4 whitespace-nowrap text-light">
+          <h3 className="text-3xl">Previous Work Experience</h3>
+          <hr className="text-4xl w-60" />
+        </div>
         <div className="flex flex-col md:flex-row pt-4 gap-4">
-          <div className="md:hidden flex flex-row">
+          <div className="md:hidden flex flex-row overflow-scroll">
             {experience.map((item, idx) => (
               <div
                 className={`${
                   idx === selected
                     ? "border-neonBlue text-neonBlue border-b-[3px] "
                     : "border-light border-b-2"
-                }  px-2  transition-all  py-2`}
+                }  px-3  transition-colors ease-in-out duration-500 py-2`}
                 onClick={() => setSelected(idx)}
                 key={idx}
               >
@@ -41,12 +61,12 @@ export default function Work() {
               </div>
             ))}
           </div>
-          <div className="hidden md:w-1/4 md:flex md:flex-col">
+          <div className="hidden md:w-[30%] md:flex md:flex-col">
             {experience.map((item, idx) => (
               <div
                 className={`${
                   idx === selected
-                    ? "border-neonBlue text-neonBlue border-l-[3px] "
+                    ? "border-neonBlue text-neonBlue border-l-[3px] bg-light bg-opacity-50"
                     : "border-light border-l-2"
                 } pl-4  transition-all  py-2`}
                 onClick={() => setSelected(idx)}
