7348035652589f1d845f8d8dab8b29d736b95e0a
Gopal Kaul <gopalkaul5@gmail.com>
2022-10-23 14:14:12 +0530

    Add Svelte and Svelte Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  site: "https://gopal-kaul.github.io",
+  integrations: [react(), tailwind(), svelte()],
+  site: "https://gopal-kaul.github.io"
 });
